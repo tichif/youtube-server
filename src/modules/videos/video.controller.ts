5a943d83ea0cae3b8b38e768ed6e62adbae97f0b
Dalzon Charles-Hébert <charlby5@gmail.com>
2022-11-03 15:16:17 -0400

    Allow Cross-Origin-Resource-Policy

--- src/modules/videos/video.controller.ts
+++ src/modules/videos/video.controller.ts
@@ -132,7 +132,7 @@ export async function streamVideoHandler(req: Request, res: Response) {
     'Accept-ranges': 'bytes',
     'Content-Length': contentLength,
     'Content-Type': `video/${video.extension}`,
-    // "Cross-Origin-resource-Policy": "cross-origin"
+    'Cross-Origin-Resource-Policy': 'cross-origin',
   };
 
   res.writeHead(StatusCodes.PARTIAL_CONTENT, headers);
